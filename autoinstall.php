fc697ed80a3118f1db4465e55bc9a3b3477d0bf1
Dirk Haun <16507691+dhaun@users.noreply.github.com>
2010-07-24 11:13:55 +0200

    Replaced old install script with autoinstall.php and set version number to 2.1

---
+++ autoinstall.php
@@ -0,0 +1,91 @@
+<?php
+
+/* Reminder: always indent with 4 spaces (no tabs). */
+// +---------------------------------------------------------------------------+
+// | NetTools Plugin 2.1                                                       |
+// +---------------------------------------------------------------------------+
+// | autoinstall.php                                                           |
+// |                                                                           |
+// | This file provides helper functions for the automatic plugin install.     |
+// +---------------------------------------------------------------------------+
+// | Copyright (C) 2008-2010 by the following authors:                         |
+// |                                                                           |
+// | Authors: Dirk Haun         - dirk AT haun-online DOT de                   |
+// |          Tom Willett       - twillett AT users DOT sourceforge DOT net    |
+// +---------------------------------------------------------------------------+
+// |                                                                           |
+// | This program is free software; you can redistribute it and/or             |
+// | modify it under the terms of the GNU General Public License               |
+// | as published by the Free Software Foundation; either version 2            |
+// | of the License, or (at your option) any later version.                    |
+// |                                                                           |
+// | This program is distributed in the hope that it will be useful,           |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
+// | GNU General Public License for more details.                              |
+// |                                                                           |
+// | You should have received a copy of the GNU General Public License         |
+// | along with this program; if not, write to the Free Software Foundation,   |
+// | Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.           |
+// |                                                                           |
+// +---------------------------------------------------------------------------+
+
+/**
+* Autoinstall API functions for the NetTools plugin
+*
+* @package NetTools
+*/
+
+/**
+* Plugin autoinstall function
+*
+* @param    string  $pi_name    Plugin name
+* @return   array               Plugin information
+*
+*/
+function plugin_autoinstall_nettools($pi_name)
+{
+    $pi_name         = 'nettools';
+    $pi_display_name = 'Nettools';
+    $pi_admin        = $pi_display_name . ' Admin';
+
+    $info = array(
+        'pi_name'         => $pi_name,
+        'pi_display_name' => $pi_display_name,
+        'pi_version'      => '2.1.0',
+        'pi_gl_version'   => '1.6.0',
+        'pi_homepage'     => 'http://www.geeklog.net/'
+    );
+
+    $groups = array(
+        $pi_admin => 'Users in this group can administer the ' . $pi_display_name . ' plugin'
+    );
+
+    $features = array(
+        'Whois.view'    => "Whois Viewer",
+        'NSLookup.view' => "NSLookup Viewer",
+        'Ping.view'     => "Ping Viewer",
+        'Tracert.view'  => "Tracert Viewer"
+    );
+
+    $mappings = array(
+        'Whois.view'    => array($pi_admin),
+        'NSLookup.view' => array($pi_admin),
+        'Ping.view'     => array($pi_admin),
+        'Tracert.view'  => array($pi_admin)
+    );
+
+    $tables = array(
+    );
+
+    $inst_parms = array(
+        'info'      => $info,
+        'groups'    => $groups,
+        'features'  => $features,
+        'mappings'  => $mappings
+    );
+
+    return $inst_parms;
+}
+
+?>
